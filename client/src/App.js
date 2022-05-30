08183b6ccd604dfdcaaacd6d46d2068aaf11dc37
skiran017 <skiran017@gmail.com>
2022-05-30 22:49:03 +0530

    feat: connect React dApp to near

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { getProducts } from "./utils/marketplace";
+import { login } from "./utils/near";
 
 function App() {
+  const account = window.walletConnection.account();
+  const [products, setProducts] = useState([]);
+  const fetchProducts = useCallback(async () => {
+    if (account.accountId) {
+      setProducts(await getProducts());
+    }
+  });
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {account.accountId ? (
+        products.forEach((product) => console.log(product))
+      ) : (
+        <button onClick={login}>CONNECT WALLET</button>
+      )}
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { initializeContract } from './utils/near';
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+window.nearInitPromise = initializeContract()
+  .then(() => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  })
+  .catch(console.error);
